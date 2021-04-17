25444a4edca25d587776cbac2cfc0a2189092c5c
MikicaNovakovic <mikica22.mn@gmail.com>
2021-04-17 21:27:09 +0200

    Finished project

---
+++ src/app/model/restaurantsList.ts
@@ -0,0 +1,11 @@
+import { Restaurant } from "./restaurantsModel";
+
+export class RestaurantsList{
+    results: Restaurant[];
+    count: number;
+
+    constructor(obj?:any){
+        this.results = obj && obj.results.map( elem => { return new Restaurant(elem)}) || [];
+        this.count = obj && obj.count || 0;
+    }
+}
